f8e2306ffedb9d285286f46a952b6e4dcb9d09f1
e.an <estateangela@gmail.com>
2024-03-06 20:56:32 +0800

    update

--- bmi.py
+++ bmi.py
@@ -2,5 +2,4 @@ x = float(input("身高Cm:"))
 y = float(input("體重Kg:"))
 
 BMI = y/(x/100)**2
-print("BMI為:",BMI)
-
+print(f'BMI:{BMI:.2f}',sep="")
---
+++ test2.py
@@ -0,0 +1,10 @@
+BMI= 26.72
+#不空隔
+print("display","你的BMI=",BMI,sep="----",end='')
+#不換行
+print("TEST",end='@@')
+print("TEST",end='@@')
+print("TEST",end='@@')
+
+#F-STRING
+print(f'display:你的bmi={BMI}')
