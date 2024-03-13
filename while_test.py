2347b92b15b9e83c0b39707bcbfcf3cddd58608a
e.an <161005688+estateangela@users.noreply.github.com>
2024-03-13 19:44:52 +0800

    update

---
+++ while_test.py
@@ -0,0 +1,24 @@
+i = 2
+
+#小於10的時候印出1-9
+
+while i <= 100:
+    print(i)
+    i += 2
+
+print('-----------------')
+    
+p = 6
+
+while p <= 100:
+    print(p)
+    p +=10
+
+print('-----------------')
+
+n = 2 
+while n <=100:
+    print(n)
+    n +=2
+    if n > 50:
+        break
