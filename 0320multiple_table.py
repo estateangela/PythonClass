3d36c6717d1f50e0e7100a134a7b6e824bb6bcb2
e.an <161005688+estateangela@users.noreply.github.com>
2024-03-27 19:35:38 +0800

    feat: new list practice for python
    use for range to draw different side triangle

--- 0320multiple_table.py
+++ 0320multiple_table.py
@@ -1,7 +1,7 @@
 #九九乘法表
 
 for i in range(1,10):
- for j in range(1,10):
-    print(f'{i}x{j}={i*j}',end='   ')
+ for j in range(1,i):
+    print(f'{i}x{j}={i*j:2d}',end='   ')
  print()    
 
---
+++ 0327.py
@@ -0,0 +1,44 @@
+#九九乘法表
+
+for i in range(1,7):
+ for j in range(1,i):
+    print(f'{i}x{j}={i*j}',end='   ')
+ print()
+
+print('--------------')
+#左下角
+ 
+N = 5
+for i in range(N):  #range(N)=range(0,N)=range(1,N+1)
+    for j in range(i+1):
+        print('*',end='')
+    print()
+
+print('--------------')    
+#左上角
+
+n=5
+for i in range(n,0,-1):
+    for j in range(i):
+         print('*',end='')
+    print()
+
+print('--------------')    
+#右下角
+
+for i in range(N):
+    for j in range(N-i): #i=1 => for j in range(4): => [0,1,2,3]
+        print(' ',end=' ')
+    for j in range(i+1):
+        print('*',end=' ')
+    print()
+    
+print('--------------')
+#右上角
+
+for i in range(n,0,-1):
+    for j in range(n-i):
+        print(' ',end=' ')
+    for j in range(i): #i=1 => for j in range(4): => [0,1,2,3]
+        print('*',end=' ')
+    print()
